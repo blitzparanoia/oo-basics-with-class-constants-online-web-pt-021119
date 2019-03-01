0b0e246de502f168a8e2d7193c62e883297415cc
Irene Saez <bluemilk44@gmail.com>
2019-03-01 21:44:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,10 +3,10 @@ class Shoe
   attr_reader :brand
   
   BRANDS = []
-
-  def initialize(brand)
+  
+   def initialize(brand)
     @brand = brand
-    BRANDS << brand unless BRAND.include?(brand)
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
